7d5ab5ad450f8fd7481c7bb83acfc5eb34081886
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-08 17:21:24 +0530

    Enhance CORS configuration and add Inngest API routes
    - Updated CORS middleware to allow requests from any origin.
    - Added routes for Inngest API to handle signup and ticket creation events.
    - Established MongoDB connection setup for the application.

--- src/index.js
+++ src/index.js
@@ -15,17 +15,22 @@ dotenv.config()
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", userRoutes);
 app.use("/api/ticket", ticketRoutes);
 
+// Inngest API
 app.use("/api/inngest", serve({
   client: inngest,
   functions: [onSignup, onTicketCreated]
 }));
 
+// MongoDB connection
 mongoose
   .connect(`${process.env.MONGO_URI}${DB_NAME}`)
   .then(() => {
