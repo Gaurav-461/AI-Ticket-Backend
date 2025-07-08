2311c300462223e87d0a773eb5934c756cc6d1b8
Gaurav-461 <mauryagaurav1612@gmail.com>
2025-07-08 14:50:02 +0530

    Initialize project with essential files: added .gitignore, package.json, package-lock.json, and main server file (index.js) with Express setup and MongoDB connection.

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constant.js";
+import cors from "cors";
+import userRoutes from "./routes/user.route.js";
+import ticketRoutes from "./routes/ticket.route.js";
+import { serve } from "inngest/express";
+import { inngest } from "./inngest/client.js";
+import { onSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/ticket", ticketRoutes);
+
+app.use("/api/inngest", serve({
+  client: inngest,
+  functions: [onSignup, onTicketCreated]
+}));
+
+mongoose
+  .connect(`${process.env.MONGO_URI}${DB_NAME}`)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () =>
+      console.log(`Server is running on http://localhost:3000`)
+    );
+  })
+  .catch((err) => console.log("❌ MongoDB connection error:- ", err));
